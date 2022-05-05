ad3f6da6315e174a93ec2e9e55f5b730af69877f
Suhail <suhailmoidin96@gmail.com>
2022-05-05 15:38:32 +0530

    first commit

---
+++ Cristian’s algorithm/Server.py
@@ -0,0 +1,27 @@
+import socket
+import datetime
+import time
+# function used to initiate the Clock Server
+def initiateClockServer():
+	s = socket.socket()
+	print("Socket successfully created")
+	# Server port
+	port = 8012
+	s.bind(('127.0.0.1', port))
+	# Start listening to requests
+	s.listen(5)
+	print("Socket is listening...")
+	# Clock Server Running forever
+	while True:
+		# Establish connection with client
+		connection, address = s.accept()
+		print('Server connected to', address)
+		# Respond the client with server clock time
+		connection.send(str(datetime.datetime.now()).encode())
+		# Close the connection with the client process
+		connection.close()
+
+# Driver function
+if __name__ == '__main__':
+	# Trigger the Clock Server
+	initiateClockServer()
